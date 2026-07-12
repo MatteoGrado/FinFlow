94135f82473df9ee434faf7235b9de75ea5cac70
MatteoGrado <matteo.grado.23@gmail.com>
2026-07-12 02:56:40 +0200

    First Commit

---
+++ src/main/java/de/grado/finflow/FinFlowApplication.java
@@ -0,0 +1,15 @@
+package de.grado.finflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinFlowApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FinFlowApplication.class, args);
+    }
+
+}
